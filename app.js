ff1a8479f796f4ff7f99c876113cd5c9da1cffeb
Snagrap <karlkarilaid04@gmail.com>
2022-02-21 09:29:34 +0200

    Added item to data structure

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ const ItemCtrl = (function() {
 		getItems: function(){
 			return data.items
 		},
+		addItem: function(name, calories){
+			let ID;
+			if(data.items.length > 0){
+				ID = data.items[data.items.length - 1].id + 1
+			} else {
+				ID = 0
+			}
+			calores = parseInt(calories);
+			newItem = new Item(ID, name, calories);
+			data.items.push(newItem);
+			return newItem
+		},
 		logData: function() {
 			return data
 		}
@@ -26,7 +38,10 @@ const ItemCtrl = (function() {
 
 const UICtrl = (function() {
 	const UISelectors = {
-		itemList: '#item-list'
+		itemList: '#item-list',
+		itemNameInput: '#item-name',
+		itemCaloriesInput: '#item-calories',
+		addBtn: '.add-btn'
 	}
 
 	return {
@@ -43,16 +58,38 @@ const UICtrl = (function() {
 			});
 
 			document.querySelector(UISelectors.itemList).innerHTML = html;
+		},
+		getSelectors: function(){
+			return UISelectors;
+		},
+		getItemInput: function(){
+			return {
+				name: document.querySelector(UISelectors.itemNameInput).value,
+				calories: document.querySelector(UISelectors.itemCaloriesInput).value
+			}
 		}
 	}
 })();
 
 const App = (function(ItemCtrl, UICtrl) {
+	const loadEventListeners = function(){
+		const UISelectors = UICtrl.getSelectors();
+		document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+	}
+	const itemAddSubmit = function(event){
+		const input = UICtrl.getItemInput()
+		if(input.name !== '' && input.calories !== ''){
+			const newItem = ItemCtrl.addItem(input.name, input.calories)
+			console.log(newItem)
+		}
+		event.preventDefault()
+	}
 	return {
 		init: function(){
 			console.log('Initializing App')
 			const items = ItemCtrl.getItems()
 			UICtrl.populateItemList(items)
+			loadEventListeners();
 		}
 	}
 })(ItemCtrl, UICtrl);
