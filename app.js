2a0a1c8dbee7d2b5691e15b61ade1494ad7c8a4a
Snagrap <karlkarilaid04@gmail.com>
2022-02-21 09:40:13 +0200

    Added item to the UI list

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const ItemCtrl = (function() {
 
 	const data = {
 		items: [
-			{id: 0, name: 'Steak Dinner', calories: 1200},
-			{id: 1, name: 'Cookie', calories: 400},
-			{id: 2, name: 'Eggs', calories: 300}
+			//{id: 0, name: 'Steak Dinner', calories: 1200},
+			//{id: 1, name: 'Cookie', calories: 400},
+			//{id: 2, name: 'Eggs', calories: 300}
 			],
 		total: 0
 	}
@@ -67,6 +67,21 @@ const UICtrl = (function() {
 				name: document.querySelector(UISelectors.itemNameInput).value,
 				calories: document.querySelector(UISelectors.itemCaloriesInput).value
 			}
+		},
+		addListItem: function(item){
+			const li = document.createElement('li');
+			li.className = 'collection-item';
+			li.id = `item-${item.id}`;
+			li.innerHTML = `<strong>${item.name}: </strong>
+			<em>${item.calories} Calories</em>
+			<a href="#" class="secondary-content">
+				<i class="edit-item fa fa-penchil"></i>
+			</a>`;
+			document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+		},
+		clearInput: function(){
+			document.querySelector(UISelectors.itemNameInput).value = '';
+			document.querySelector(UISelectors.itemCaloriesInput).value = '';
 		}
 	}
 })();
@@ -80,7 +95,8 @@ const App = (function(ItemCtrl, UICtrl) {
 		const input = UICtrl.getItemInput()
 		if(input.name !== '' && input.calories !== ''){
 			const newItem = ItemCtrl.addItem(input.name, input.calories)
-			console.log(newItem)
+			UICtrl.addListItem(newItem)
+			UICtrl.clearInput();
 		}
 		event.preventDefault()
 	}
